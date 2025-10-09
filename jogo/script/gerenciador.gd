20b5855a311f70880586d6abc45fbcdea021370a
Th1ago-V <thiagomacielmoura2009@gmail.com>
2025-10-09 14:44:15 -0300

    coisas da minha fase?

--- jogo/script/gerenciador.gd
+++ jogo/script/gerenciador.gd
@@ -1,8 +1,15 @@
 extends Node
 
+# Cena 1
 signal kill
 signal morte
 signal explode_asteroide
 var health := 3.0
+
+# Cena 2
 signal troca_cena
 var dialogo = false
+
+# Cena 4
+signal Sp
+signal Sg
